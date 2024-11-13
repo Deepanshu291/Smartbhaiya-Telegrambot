527c28807af09ae51cbb3d14bd4b73f1e322907c
Deepanshu <deepanshu2912001@gmail.com>
2024-11-13 19:35:20 +0530

    fix bugs

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import os
 from flask import Flask
 import threading
 import asyncio
 from bot import main
+import requests
+import time
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def start():
@@ -15,12 +22,22 @@ def start_bot():
     loop.run_until_complete(main())
 
 def run_flask():
-    app.run(host='0.0.0.0',port=8000)
+    port = int(os.getenv("PORT",8000))
+    app.run(host='0.0.0.0',port=port)
+
+def ping_self():
+    while True:
+        try:
+            requests.get("https://smartbhaiya-telegrambot.onrender.com/")
+        except Exception as e:
+            print("Ping failed:", e)
+        time.sleep(300)
 
 if __name__ == '__main__':
     flaskThread = threading.Thread(target=run_flask)
     flaskThread.start()
 
     start_bot()
+
 
 
